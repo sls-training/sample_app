696674500c79957fb4ef093fc3e861d12e2920f1
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-01-20 19:32:27 +0900

    6.2終了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,7 +28,10 @@ RSpec.describe User, type: :model do
   end
 
   it '有効なメールアドレスの受け入れ' do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses = %w[
+      user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn
+    ]
     valid_addresses.each do |valid_address|
       user.email = valid_address
       expect(user).to be_valid, "#{valid_address.inspect} is valid"
@@ -36,10 +39,27 @@ RSpec.describe User, type: :model do
   end
 
   it '無効なメールアドレスの拒否' do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses = %w[
+      user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com foo@bar..com
+    ]
     invalid_addresses.each do |invalid_address|
       user.email = invalid_address
       expect(user).not_to be_valid, "#{invalid_address.inspect} is invalid"
     end
+  end
+
+  it '重複するメールアドレスの拒否' do
+    duplicate_user = user.dup
+    duplicate_user.email = user.email.upcase
+    user.save
+    expect(duplicate_user).not_to be_valid
+  end
+
+  it 'メールアドレスを小文字にする' do
+    user.email = user.email.upcase
+    user.save
+    user.reload
+    expect(user.email).to eq user.email.downcase
   end
 end

5dabcdc5acb5a6ed1f8df6137208dda6c85db6bb
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-01-20 18:42:16 +0900

    6.2.4終了

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,4 +26,20 @@ RSpec.describe User, type: :model do
     user.email = 'a' * 244 + '@example.com'
     expect(user).not_to be_valid
   end
+
+  it '有効なメールアドレスの受け入れ' do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      user.email = valid_address
+      expect(user).to be_valid, "#{valid_address.inspect} is valid"
+    end
+  end
+
+  it '無効なメールアドレスの拒否' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    invalid_addresses.each do |invalid_address|
+      user.email = invalid_address
+      expect(user).not_to be_valid, "#{invalid_address.inspect} is invalid"
+    end
+  end
 end

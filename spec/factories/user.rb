7d6b1eb36f28564b268b9c807a3f832dcc5de9da
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-03-10 19:37:58 +0900

    8.2.4終了

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,5 +4,12 @@ FactoryBot.define do
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { 'foobar' }
     password_confirmation { 'foobar' }
+
+    trait :michael do
+      name { 'Michael Example' }
+      email { 'michael@example.com' }
+      password { 'password' }
+      password_confirmation { 'password' }
+    end
   end
 end
--- spec/requests/users_login_test_spec.rb
+++ spec/requests/users_login_test_spec.rb
@@ -1,24 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe 'LoginPage', type: :request do
-  it 'ログインページにアクセスできること' do
-    get login_path
-    expect(response).to have_http_status(:ok)
+  let(:testuser) { FactoryBot.create(:user, :michael) }
+
+  describe 'GET Login Test' do
+    it 'ログインページにアクセスできること' do
+      get login_path
+      expect(response).to have_http_status(:ok)
+    end
   end
 
-  context '無効なデータが送信された場合' do
-    subject { post login_path, params: { session: { email: '', password: '' } } }
+  describe 'POST Login Test' do
+    context '無効なデータが送信された場合' do
+      subject { post login_path, params: { session: { email: '', password: '' } } }
 
-    it 'フラッシュメッセージが表示されること' do
-      get login_path
-      subject
-      expect(response).to have_http_status(:ok)
-      expect(flash[:danger]).to be_truthy
+      it 'フラッシュメッセージが表示されること' do
+        get login_path
+        subject
+        expect(response).to have_http_status(:ok)
+        expect(flash[:danger]).to be_truthy
+      end
+
+      it '別のページ(Home)にフラッシュメッセージが表示されていないこと' do
+        get root_path
+        expect(flash[:danger]).to be_falsey
+      end
     end
 
-    it '別のページ(Home)にフラッシュメッセージが表示されていないこと' do
-      get root_path
-      expect(flash[:danger]).to be_falsey
+    context '有効なデータが送信された場合' do
+      subject { post login_path, params: { session: { email: testuser.email, password: testuser.password } } }
+
+      it 'ログイン用リンクが非表示になっていること' do
+        get login_path
+        subject
+        follow_redirect!
+        expect(response).to have_http_status(:ok)
+        expect(response.body).not_to include(login_path)
+      end
+
+      it 'ログアウト用、プロフィール用リンクが表示されていること' do
+        get login_path
+        subject
+        follow_redirect!
+        expect(response.body).to include(logout_path)
+        expect(response.body).to include(user_path)
+      end
     end
   end
 end

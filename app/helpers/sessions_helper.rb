ca785b954f2cce68bf6e0b4bb7da0b9940589b74
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-03-17 19:27:44 +0900

    9.1.2終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,26 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if (user_id = session[:user])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticate?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
   # 現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)

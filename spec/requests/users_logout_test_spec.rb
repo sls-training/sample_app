50bc02a1ef27d9544200574093be84ab6f65ca0d
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-03-17 19:08:36 +0900

    テストの記述を修正

--- spec/requests/users_logout_test_spec.rb
+++ spec/requests/users_logout_test_spec.rb
@@ -4,18 +4,16 @@ RSpec.describe 'ログアウトテスト', type: :request do
   let(:testuser) { FactoryBot.create(:user, :michael) }
 
   context 'ログアウトボタンを押した場合' do
-    subject { post login_path, params: { session: { email: testuser.email, password: testuser.password } } }
+    before do
+      post login_path, params: { session: { email: testuser.email, password: testuser.password } }
+    end
+
+    subject do
+      delete logout_path
+    end
 
     it 'ログアウトできること' do
-      # 最初にログインし、ログイン状態であるかの確認
       subject
-      expect(is_logged_in?).to be_truthy
-
-      # ログアウト処理、確認
-      delete logout_path
-      expect(is_logged_in?).not_to be_truthy
-
-      # ルートページに移動し、それぞれのリンク表示の確認
       redirect_to root_path
       follow_redirect!
       expect(response).to have_http_status(:ok)

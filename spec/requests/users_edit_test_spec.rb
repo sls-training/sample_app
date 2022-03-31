253a369869f421b5a9ab829a6d1c981a6bba5895
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-03-31 14:15:43 +0900

    10.1終了

--- spec/requests/users_edit_test_spec.rb
+++ spec/requests/users_edit_test_spec.rb
@@ -15,17 +15,44 @@ RSpec.describe 'EditPage', type: :request do
       subject { patch user_path(testuser), params: { user: { name: '', email: 'foo@invalid', password: 'foo', password_confirmation: 'bar' } } }
 
       it 'editページが再描写されること' do
-        get edit_user_path(testuser)
         subject
         redirect_to edit_user_path(testuser)
         expect(response).to have_http_status(:ok)
       end
 
       it '「The form contains 4 errors」が表示されていること' do
-        get edit_user_path(testuser)
         subject
         redirect_to edit_user_path(testuser)
         expect(response.body).to include('The form contains 4 errors')
+      end
+    end
+
+    context '有効な値の場合' do
+      subject do
+        @name = 'Foo Bar'
+        @email = 'foo@bar.com'
+        patch user_path(testuser), params: { user: { name: @name, email: @email, password: '', password_confirmation: '' } }
+      end
+
+      it 'flashメッセージが表示されていること' do
+        subject
+        redirect_to user_path(testuser)
+        follow_redirect!
+        expect(flash[:success]).to be_truthy
+      end
+
+      it 'プロフィールページにリダイレクトされること' do
+        subject
+        redirect_to user_path(testuser)
+        follow_redirect!
+        expect(response).to have_http_status(:ok)
+      end
+
+      it '更新されること' do
+        subject
+        testuser.reload
+        expect(testuser.name).to eq @name
+        expect(testuser.email).to eq @email
       end
     end
   end

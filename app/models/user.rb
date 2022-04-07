2b6f2cba33b3c3d20d7d8bbc7c3b31fba700361b
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-04-07 14:23:47 +0900

    11.2.2終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   attr_accessor :remember_token, :activation_token
 
-  before_save :downcase_email
+  before_save   :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
@@ -49,7 +49,7 @@ class User < ApplicationRecord
 
     # 有効化トークンとダイジェストを作成及び代入する
     def create_activation_digest
-      self.activation_token = User.new_token
+      self.activation_token  = User.new_token
       self.activation_digest = User.digest(activation_token)
     end
 end

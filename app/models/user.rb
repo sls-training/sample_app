746e78ae0d1f2f11afbe196a852e62b9bae1b610
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-04-07 12:28:02 +0900

    11.1終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
 
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
@@ -28,6 +31,7 @@ class User < ApplicationRecord
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(remember_token)
     return false if remember_digest.nil?
+
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
@@ -35,4 +39,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+  private
+
+    # メールアドレスを全て小文字化する
+    def downcase_email
+      email.downcase!
+    end
+
+    # 有効化トークンとダイジェストを作成及び代入する
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,6 +4,8 @@ FactoryBot.define do
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { 'foobar' }
     password_confirmation { 'foobar' }
+    activated { true }
+    activated_at { Time.zone.now }
 
     trait :michael do
       name { 'Michael Example' }
@@ -19,6 +21,8 @@ FactoryBot.define do
     password { 'password' }
     password_confirmation { 'password' }
     admin { true }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 
   factory :many_user, class: 'User' do
@@ -26,5 +30,7 @@ FactoryBot.define do
     sequence(:email) { |n| "manyuser_#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 end

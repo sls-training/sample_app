904e9634244a5180e3dde4ed0e8c80ae2a58852f
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-04-07 16:37:37 +0900

    11.2.3終了

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,32 +1,54 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
+  describe 'account_activation' do
+    let(:user) { FactoryBot.create(:user, :michael) }
+    let(:mail) { UserMailer.account_activation(user) }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+    context 'headers' do
+      before do
+        user.activation_token = User.new_token
+      end
+
+      it '件名が「Account activation」であること' do
+        expect(mail.subject).to eq('Account activation')
+      end
+
+      it '宛先が「to@example.org」であること' do
+        expect(mail.to).to eq([user.email])
+      end
+
+      it '送信元が「noreply@example.com」であること' do
+        expect(mail.from).to eq(['noreply@example.com'])
+      end
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    context 'bodyをレンダリングすること' do
+      it 'メール本文にユーザーネームが含まれていること' do
+        expect(mail.body.encoded).to match(user.name)
+      end
+
+      it 'メール本文に「user.activation_token」が含まれていること' do
+        expect(mail.body.encoded).to match(user.activation_token)
+      end
+
+      it 'メール本文にユーザーのemailが含まれていること' do
+        expect(mail.body.encoded).to match(CGI.escape(user.email))
+      end
     end
   end
 
-  describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
+  # describe 'password_reset' do
+  #   let(:mail) { UserMailer.password_reset }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
+  #   it 'headersをレンダリングすること' do
+  #     expect(mail.subject).to eq('Password reset')
+  #     expect(mail.to).to eq(['to@example.org'])
+  #     expect(mail.from).to eq(['from@example.com'])
+  #   end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
-    end
-  end
-
+  #   it 'bodyをレンダリングすること' do
+  #     expect(mail.body.encoded).to match('Hi')
+  #   end
+  # end
 end

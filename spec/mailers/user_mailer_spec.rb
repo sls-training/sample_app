1f50452d6750a445ff560aba992649fab6d46f30
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-04-11 16:19:16 +0900

    12.2終了

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe UserMailer, type: :mailer do
       end
     end
 
-    context 'bodyをレンダリングすること' do
+    context 'body' do
       it 'メール本文にユーザーネームが含まれていること' do
         expect(mail.body.encoded).to match(user.name)
       end
@@ -38,17 +38,36 @@ RSpec.describe UserMailer, type: :mailer do
     end
   end
 
-  # describe 'password_reset' do
-  #   let(:mail) { UserMailer.password_reset }
+  describe 'password_reset' do
+    let(:user) { FactoryBot.create(:user, :michael) }
+    let(:mail) { UserMailer.password_reset(user) }
 
-  #   it 'headersをレンダリングすること' do
-  #     expect(mail.subject).to eq('Password reset')
-  #     expect(mail.to).to eq(['to@example.org'])
-  #     expect(mail.from).to eq(['from@example.com'])
-  #   end
+    before do
+      user.reset_token = User.new_token
+    end
 
-  #   it 'bodyをレンダリングすること' do
-  #     expect(mail.body.encoded).to match('Hi')
-  #   end
-  # end
+    context 'headers' do
+      it '件名が「Password reset」であること' do
+        expect(mail.subject).to eq('Password reset')
+      end
+
+      it '宛先が「to@example.org」であること' do
+        expect(mail.to).to eq([user.email])
+      end
+
+      it '送信元が「noreply@example.com」であること' do
+        expect(mail.from).to eq(['noreply@example.com'])
+      end
+    end
+
+    context 'body' do
+      it 'メール本文に「user.reset_token」が含まれていること' do
+        expect(mail.body.encoded).to match(user.reset_token)
+      end
+
+      it 'メール本文にユーザーのemailが含まれていること' do
+        expect(mail.body.encoded).to match(CGI.escape(user.email))
+      end
+    end
+  end
 end

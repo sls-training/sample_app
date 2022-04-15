4ba7d1066883c01b9942f7c7bc79a7dc5e2da4ec
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-04-15 13:27:27 +0900

    14.1.3終了

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:test_user) { FactoryBot.create(:user, :michael) }
+  let(:other_user) { FactoryBot.create(:user, :michael, name: 'archer', email: 'archer@example.com') }
+  let(:relationship) { Relationship.new(follower_id: test_user.id, followed_id: other_user.id) }
+
+  it '有効であること' do
+    expect(relationship).to be_valid
+  end
+
+  it 'follower_idが必要なこと' do
+    relationship.follower_id = nil
+    expect(relationship).not_to be_valid
+  end
+
+  it 'followed_idが必要なこと' do
+    relationship.followed_id = nil
+    expect(relationship).not_to be_valid
+  end
 end

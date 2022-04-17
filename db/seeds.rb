a433aec55e633d5291f96e8b844fee8bdc19159f
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-04-18 08:30:34 +0900

    14.2.1終了

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,8 @@ User.create!(name:  "Example User",
 name  = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
-User.create!(name:  name,
+User.create!(
+  name:  name,
       email: email,
       password:              password,
       password_confirmation: password,
@@ -28,3 +29,11 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# 以下のリレーションシップを作成する
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }

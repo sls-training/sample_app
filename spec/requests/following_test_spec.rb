3756c7e33e3730248d5ee924d4a7bf31f4de1c66
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-04-18 15:54:00 +0900

    14.2終了

--- spec/requests/following_test_spec.rb
+++ spec/requests/following_test_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'MicropostsInterfaceTest', type: :request do
   let(:test_user) { FactoryBot.create(:user, :michael) }
-  let(:other) { FactoryBot.create(:user, name: 'lana') }
-  let(:other2) { FactoryBot.create(:user, name: 'malory') }
-  let(:other3) { FactoryBot.create(:user, name: 'archer') }
+  let(:test_user2) { FactoryBot.create(:user, name: 'lana') }
+  let(:test_user3) { FactoryBot.create(:user, name: 'malory') }
+  let(:test_user4) { FactoryBot.create(:user, name: 'archer') }
 
   before do
     log_in_as(test_user)
-    test_user.follow(other2)
-    test_user.follow(other3)
-    other2.follow(test_user)
-    other.follow(test_user)
+    test_user.follow(test_user3)
+    test_user.follow(test_user4)
+    test_user3.follow(test_user)
+    test_user2.follow(test_user)
   end
 
   context 'following page' do
@@ -31,8 +31,36 @@ RSpec.describe 'MicropostsInterfaceTest', type: :request do
       expect(test_user.following).not_to be_empty
       assert_match test_user.followers.count.to_s, response.body
       test_user.followers.each do |test_user|
-        #assert_select 'a[href=?]', user_path(test_user)
+        assert_select 'a[href=?]', user_path(test_user)
       end
+    end
+  end
+
+  describe 'create' do
+    let(:other_user) { FactoryBot.create(:user, name: 'bob') }
+
+    before do
+      log_in_as(test_user)
+    end
+
+    it 'フォローしたら1件増えること' do
+      expect { post relationships_path, params: { followed_id: other_user.id } }.to change(Relationship, :count).by(1)
+    end
+
+    it 'Ajaxでフォローできること' do
+      expect { post relationships_path, xhr: true, params: { followed_id: other_user.id } }.to change(Relationship, :count).by(1)
+    end
+
+    it 'フォロー解除したら1件減ること' do
+      test_user.follow(other_user)
+      relationship = test_user.active_relationships.find_by(followed_id: other_user.id)
+      expect { delete relationship_path(relationship) }.to change(Relationship, :count).by(-1)
+    end
+
+    it 'Ajaxでフォロー解除できること' do
+      test_user.follow(other_user)
+      relationship = test_user.active_relationships.find_by(followed_id: other_user.id)
+      expect { delete relationship_path(relationship), xhr: true }.to change(Relationship, :count).by(-1)
     end
   end
 end

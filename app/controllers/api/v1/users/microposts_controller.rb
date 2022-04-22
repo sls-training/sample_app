89cb6a39a6d2a6daafbbb5b3bb45054f380ae97d
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-04-22 15:12:55 +0900

    API開発

---
+++ app/controllers/api/v1/users/microposts_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    module Users
+      class MicropostsController < Api::ErrorRenderController
+        def index
+          @user = User.find(params[:user_id]) # ユーザーIDの指定
+          @page = params[:page] # ページネーションのページの指定。指定がなければ1
+          @per_page = params[:per_page] || 30 # マイクロポストの表示数を指定。指定がなければ30
+          @microposts = @user.microposts.paginate(page: @page, per_page: @per_page)
+          render json: [microposts: @microposts]
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,26 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
   resources :users do
     member do
       get :following, :followers
     end
   end
+
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
+
+  namespace 'api', format: 'json' do
+    namespace 'v1' do
+      resources :users do
+        scope module: :users do
+          resources :microposts, only: %i[index]
+        end
+      end
+    end
+  end
 end

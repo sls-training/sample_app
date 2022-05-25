a85c7b03049285321cca75c3f72c9a29c3a5a157
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-05-25 18:19:40 +0900

    有効期限の記述を変更

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,4 +1,5 @@
 require 'base64'
+require 'active_support/time'
 
 class Api::V1::AuthController < Api::ErrorRenderController
   protect_from_forgery
@@ -10,8 +11,8 @@ class Api::V1::AuthController < Api::ErrorRenderController
       # user tableのauth_tokenに入れる（末尾に謎の改行コードが入ってしまうので.chomp!で改行コードを削除）
       user.auth_token = auth.chomp!
       # user tableのexpiration_dateに有効期限（現在時刻から30分後）を入れる
-      date = Time.now + 30 * 60
-      user.expiration_date = date
+      date = Time.now.since(30.minutes)
+      user.expiration_at = date
       # DBの変更を保存
       user.save
       render json: { "success": { "auth": auth, "expiration_date": date.to_s(:db) } }

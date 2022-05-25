be600324a2088b1c63e71a97573ae2a74269ff4c
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-05-25 16:17:07 +0900

    時刻の処理・表記を修正

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -10,10 +10,11 @@ class Api::V1::AuthController < Api::ErrorRenderController
       # user tableのauth_tokenに入れる（末尾に謎の改行コードが入ってしまうので.chomp!で改行コードを削除）
       user.auth_token = auth.chomp!
       # user tableのexpiration_dateに有効期限（現在時刻から30分後）を入れる
-      user.expiration_date = Time.now + 30 * 60
+      date = Time.now + 30 * 60
+      user.expiration_date = date
       # DBの変更を保存
       user.save
-      render json: { "success": { "auth": auth, "expiration_date": Time.now + 30 * 60 } }
+      render json: { "success": { "auth": auth, "expiration_date": date.to_s(:db) } }
     else
       render json: { "error": { "status": 401, "message": 'Unauthorized' } }
     end

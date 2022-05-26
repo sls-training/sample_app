370510d003f6b08accda2317a36f618463e6b2b1
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-05-26 11:36:19 +0900

    有効期限のkeyの名前変更・表示形式変更

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -10,12 +10,12 @@ class Api::V1::AuthController < Api::ErrorRenderController
       auth = Base64.encode64("#{user.id.to_s}:#{User.new_token}")
       # user tableのauth_tokenに入れる（末尾に謎の改行コードが入ってしまうので.chomp!で改行コードを削除）
       user.auth_token = auth.chomp!
-      # user tableのexpiration_dateに有効期限（現在時刻から30分後）を入れる
+      # user tableのexpiration_atに有効期限（現在時刻から30分後）を入れる
       date = Time.now.since(30.minutes)
       user.expiration_at = date
       # DBの変更を保存
       user.save
-      render json: { "success": { "auth": auth, "expiration_date": date.to_s(:db) } }
+      render json: { "success": { "auth": auth, "expiration_at": date.iso8601 } }
     else
       render status: 401, json: { "error": { "status": 401, "message": 'Unauthorized' } }
     end
